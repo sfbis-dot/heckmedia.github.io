7000f0c078d6bc6c4f23ac7d64797821fb40c78d
taskylizard <75871323+taskylizard@users.noreply.github.com>
2024-08-16 04:10:28 +0000

    fix: only add component on non level 2 headings

--- docs/.vitepress/markdown/headers.ts
+++ docs/.vitepress/markdown/headers.ts
@@ -5,7 +5,8 @@ const titles = Object.keys(headers).map((key) => headers[key].title)
 
 export const headersPlugin = (md: MarkdownRenderer) => {
   md.renderer.rules.heading_open = (tokens, idx, options, env, self) => {
-    if (!titles.includes(env.frontmatter.title))
+    const level = tokens[idx].tag.slice(1)
+    if (!titles.includes(env.frontmatter.title) || level !== '2')
       return self.renderToken(tokens, idx, options)
     // Open a flex container before the heading
     return `<div class="flex items-center gap-2">${self.renderToken(tokens, idx, options)}`
@@ -15,7 +16,8 @@ export const headersPlugin = (md: MarkdownRenderer) => {
   md.renderer.rules.heading_close = (tokens, idx, options, env, self) => {
     const result = self.renderToken(tokens, idx, options)
     const heading = tokens[idx - 1]
-    if (!titles.includes(env.frontmatter.title)) return result
+    const level = tokens[idx].tag.slice(1)
+    if (!titles.includes(env.frontmatter.title) || level !== '2') return result
 
     return `<Feedback heading="${heading.content}" />${result}</div>`
   }
