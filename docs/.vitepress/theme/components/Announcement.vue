97439630f350282fe4189e6f28cde74dfb4dbaf2
taskylizard <75871323+taskylizard@users.noreply.github.com>
2024-08-16 04:02:22 +0000

    chore: format

--- docs/.vitepress/theme/components/Announcement.vue
+++ docs/.vitepress/theme/components/Announcement.vue
@@ -3,8 +3,11 @@ const { frontmatter } = useData()
 </script>
 
 <template>
-  <a v-if="frontmatter.hero.prelink" :href="frontmatter.hero.prelink.link"
-    class="inline-flex items-center rounded-lg bg-[var(--vp-c-default-soft)] px-4 py-1 text-sm font-semibold mb-3">
+  <a
+    v-if="frontmatter.hero.prelink"
+    :href="frontmatter.hero.prelink.link"
+    class="inline-flex items-center rounded-lg bg-[var(--vp-c-default-soft)] px-4 py-1 text-sm font-semibold mb-3"
+  >
     {{ frontmatter.hero.prelink.title }}
   </a>
 </template>
